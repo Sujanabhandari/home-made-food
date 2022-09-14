b9bfc3d34c5c12442d9ed86fd831567bd151513b
Sujana Bhandari <sujana6589@gmail.com>
2022-09-14 21:38:35 +0200

    Material UI Implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
-import Register from './Components/Register';
+import SignUp from './Components/Register';
+import Landing from './Components/Landing';
+import { NavLink, Routes, Route, Link } from "react-router-dom";
+import FoodCard from './Components/FoodCard';
+import ResponsiveAppBar from './Components/Navbar';
+
 function App() {
   return (
     <div className="App">
-      <Register />
+      {/* <SignUp /> */}
+      <ResponsiveAppBar />
+      <FoodCard />
+      <Routes>
+      <Route
+          path="signup"
+          element={<SignUp/>}
+        />
+      </Routes>
     </div>
   );
 }
